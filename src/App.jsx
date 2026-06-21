b63c21139745a2c65782adf069aa0e35cd395979
Mongkulratanak <mongkulratanak013@gmail.com>
2026-06-21 17:13:08 +1000

    Add /recipe/new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from './pages/HomePage'
+import RecipeFormPage from './pages/RecipeFormPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path ="/" element={<HomePage />} />
+        <Route path ="/recipe/new" element={<RecipeFormPage />} />
       </Routes>
     </BrowserRouter>  
   )
---
+++ src/pages/RecipeFormPage.jsx
@@ -0,0 +1,9 @@
+function RecipeFormPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Add / Edit Recipe</h1>
+            <p className="text-gray-500 mt-2">Form goes here.</p>
+        </div>
+    )
+}
+export default RecipeFormPage
